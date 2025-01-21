89f8a78c1a5d6f7740d4b73454548a6e9386a7df
Arvind <64798791+Arv100@users.noreply.github.com>
2025-01-21 16:54:32 +0530

    Update settings.py

--- appstore/settings.py
+++ appstore/settings.py
@@ -16,7 +16,7 @@ from datetime import timedelta
 import os
 from os import getenv
 
-load_dotenv()
+# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
